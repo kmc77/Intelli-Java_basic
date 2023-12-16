ac3fa4afef7c24fdb1b92518cee19f9f8f6092d9
kmc77 <k_minchoi@naver.com>
2023-12-16 18:27:24 +0900

    생성자, 패키지 이해

--- src/class1/ClassStart1.java
+++ src/class1/ClassStart1.java
@@ -13,8 +13,8 @@ public class ClassStart1 {
         int student2Grade = 80;
 
 
-        System.out.println("이름: " + student1Name + "나이: " + student1Age + "나이: " + student1Grade);
-        System.out.println("이름: " + student2Name + "나이: " + student2Age + "나이: " + student2Grade);
+        System.out.println("이름: " + student1Name + "나이: " + student1Age + "성적: " + student1Grade);
+        System.out.println("이름: " + student2Name + "나이: " + student2Age + "성적: " + student2Grade);
 
     }
 }
--- src/class1/ClassStart3.java
+++ src/class1/ClassStart3.java
@@ -3,12 +3,12 @@ package class1;
 public class ClassStart3 {
     public static void main(String[] args) {
         Student student1;
-        student1 = new Student();
+        student1 = new Student(); //x001
         student1.name = "학생1";
         student1.age = 15;
         student1.grade = 90;
 
-        Student student2 = new Student();
+        Student student2 = new Student(); //002
         student2.name = "학생2";
         student2.age = 16;
         student2.grade = 80;
